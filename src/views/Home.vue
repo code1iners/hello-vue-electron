f65be849daf48ef43c8643ff7ce3a1b37cb029c5
WONI <codeliner96@fixelsoft.com>
2022-05-26 10:18:52 +0900

    Updated home text.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="h-screen flex items-center justify-center text-indigo-500">
-    <h1 class="text-3xl tracking-wider">Hello Electron!</h1>
+  <div class="h-screen flex flex-col items-center justify-center">
+    <h1 class="text-3xl tracking-wider text-indigo-500">Hello Electron!</h1>
+    <p class="mt-5">
+      This sample app included <strong>Vue3 + Typescript + TailwindCSS</strong>
+    </p>
   </div>
 </template>
 
